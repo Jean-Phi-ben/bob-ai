8d524e74128644b7bebc62c88a237741c83763c3
Louis Alexandre Richoux <louisalex.richoux@gmail.com>
2025-11-25 12:43:00 +0100

    Seed part done for L-A

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,9 @@
 class Project < ApplicationRecord
   belongs_to :user
+
+  validates :status, inclusion: { in: ["ongoing", "finished",]}
+  validates :category, inclusion: { in: ["Carpentry", "Electricity", "Plumbing"]}
+  validates :tools, inclusion: { in: ["Hammer", "Screwdriver", "Axe","Saw","Drill","Tape measure"]}
+  validates :materials, inclusion: { in: ["Screw", "Nail", "Plywood", "Washers", "Anchors", "Varnish", "Pipes", "Gaskets", "Wires", "Switches", "Outlets", "Light", "Cables"]}
+
 end
