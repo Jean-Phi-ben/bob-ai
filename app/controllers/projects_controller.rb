a0af8c74d5eebcdb64ddf7d4d90381eca924a751
Louis Alexandre Richoux <louisalex.richoux@gmail.com>
2025-11-25 15:40:39 +0100

    solved error

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   end
 
   def create
-     @project = Project.new(project_params)
+    @project = Project.new(project_params)
   if @project.save
     render :create
   else
