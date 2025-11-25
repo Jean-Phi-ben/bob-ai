89d42e9cf11997fba7774cbb0505f718ad26f723
kamal soilihi <m.soilihikamal@gmail.com>
2025-11-25 16:39:45 +0100

    devise login

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,9 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+  def index
+    @projects = Project.all
+  end
 
   def create
      @project = Project.new(project_params)
