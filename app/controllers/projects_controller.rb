972d6c453f506b31b954c61d8c3b705c0f456930
Leopold Gosse <leopoldgosse@icloud.com>
2025-11-25 17:53:09 +0100

    page new et index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,21 +1,23 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!
   def new
     @project = Project.new
+    @project.user = current_user
   end
 
   def create
      @project = Project.new(project_params)
-  if @project.save!
-    raise
-    render :create
-  else
+     @project.user = current_user
+      if @project.save
+      #TODO: on appelle l'IA avec le prompt du user
+      redirect_to project_messages_path(@project), notice: "Project created successfully."
+
+      else
     render :new, status: :unprocessable_entity
-  end
+    end
   end
 
   def show
-    raise
-    @project = Project.find(params[:id])
   end
 
   def update
@@ -27,8 +29,13 @@ class ProjectsController < ApplicationController
   private
 
   def project_params
-    params.require(:project).permit(:title, :category, :status, :tools, :materials, :methodology, :prompt)
+    params.require(:project).permit(
+      :title,
+      :category,
+      :status,
+      :prompt,
+      tools: [],      # 👈 array
+      materials: []
+    )
   end
-
-
 end
